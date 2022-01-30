10ffd4c21c394c3071fc85cf310e507eac17d065
Admin User <admin@CyberPower.localdomain>
2022-01-30 06:57:34 -0700

    Update Info Modal to use turtle examples.

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,41 +10,41 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the WORDLE in 6 tries. After each guess, the color of the tiles
-        will change to show how close your guess was to the word.
+        Guess the TURDLE animation sequence in 6 tries. After each guess, the color of the tiles
+        will change to show how close your guess was to the sequence.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="W" status="correct" />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="Q" status="correct" />
+        <Cell value="W" />
+        <Cell value="D" />
+        <Cell value="F" />
+        <Cell value="Z" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The <strong>1st Turtle</strong> is in the sequence and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell value="L" status="present" />
         <Cell value="O" />
         <Cell value="T" />
+        <Cell value="H" status="present" />
+        <Cell value="S" />
+        <Cell value="K" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The <strong>3rd Turtle</strong> is in the sequence but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="N" />
+        <Cell value="X" />
+        <Cell value="J" />
+        <Cell value="I" status="absent" />
+        <Cell value="F" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The <strong>4th Turtle</strong> is not in the sequence in any spot.
       </p>
     </BaseModal>
   )
