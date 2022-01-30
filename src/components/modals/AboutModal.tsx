373812cf2ba4ce21b00395e91ae1a278281c6a5b
Admin User <admin@CyberPower.localdomain>
2022-01-30 11:56:14 -0700

    Update About modal links and add Discord iframe.

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,7 +9,14 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is a clone of the game Wordle developed by{' '}
+        This is an{' '}
+        <a
+          href="https://github.com/SWalls/turdle"
+          className="underline font-bold"
+        >
+          open source
+        </a>
+        {' '}clone of the game Wordle developed by{' '}
         <a
           href="https://sushi2.games"
           className="underline font-bold"
@@ -22,7 +29,23 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
           className="underline font-bold"
         >
           here
+        </a>. Turtle animation by{' '}
+        <a
+          href="https://tenor.com/users/totallyturtley"
+          className="underline font-bold"
+        >
+          totallyturtley
+        </a>.
+      </p>
+      <p className="mt-4 text-sm text-gray-500 dark:text-gray-300">
+        Developed for{' '}
+        <a
+          href="https://globalgamejam.org"
+          className="underline font-bold"
+        >
+          Global Game Jam
         </a>
+        {' '}2022.
       </p>
     </BaseModal>
   )
