6a2f1b5cf1e62bce87bdc4fc7fdacce73c68f969
Admin User <admin@CyberPower.localdomain>
2022-01-29 22:23:13 -0700

    Fix modal dark mode.

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
-      <p className="text-sm text-gray-500">
+      <p className="text-sm text-gray-500 dark:text-gray-300">
         This is a clone of the game Wordle developed by{' '}
         <a
           href="https://sushi2.games"
