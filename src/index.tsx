a33f91bcb2cad8551ac4a4e2218ad8ea1c0e8f52
soeren <6632279+dinosoeren@users.noreply.github.com>
2023-02-20 07:54:57 +0000

    Init GA with react-ga4

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 import { AlertProvider } from './context/AlertContext'
+import { initGA, sendVitalsToGA } from './lib/browser'
 import reportWebVitals from './reportWebVitals'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
@@ -22,7 +23,9 @@ ReactDOM.render(
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register()
 
+initGA()
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals(sendVitalsToGA)
--- src/lib/browser.ts
+++ src/lib/browser.ts
@@ -1,3 +1,5 @@
+import assert from 'assert'
+import ReactGA from 'react-ga4'
 import { UAParser } from 'ua-parser-js'
 
 const inAppBrowserNames = [
@@ -15,4 +17,59 @@ const browser = parser.getBrowser()
 
 export const isInAppBrowser = () => {
   return inAppBrowserNames.indexOf(browser.name ?? '') > -1
+}
+
+export function isProd() {
+  return process.env.NODE_ENV === 'production'
+}
+
+/** Set @param debug `true` to initialize GA in dev mode (why tho?) */
+export function initGA({ debug }: { debug?: boolean } = {}) {
+  if (!debug && !isProd()) return
+  assert(process.env.REACT_APP_GOOGLE_MEASUREMENT_ID, 'Missing GA ID')
+  ReactGA.initialize(process.env.REACT_APP_GOOGLE_MEASUREMENT_ID)
+}
+
+/** Set @param debug `true` to send to GA in dev mode (why tho?) */
+export function sendToAnalytics({
+  category,
+  action,
+  value,
+  label,
+  nonInteraction,
+  debug,
+}: {
+  category: string
+  action: string
+  value: number
+  label: string
+  nonInteraction: boolean
+  debug?: boolean
+}) {
+  if (!debug && !isProd()) return
+  ReactGA.event({
+    category,
+    action,
+    value: Math.round(value), // values must be integers
+    label,
+    nonInteraction,
+  })
+}
+
+export function sendVitalsToGA({
+  id,
+  name,
+  value,
+}: {
+  id: string
+  name: string
+  value: number
+}) {
+  sendToAnalytics({
+    category: 'Web Vitals',
+    action: name,
+    value: name === 'CLS' ? value * 1000 : value,
+    label: id,
+    nonInteraction: true, // avoids affecting bounce rate
+  })
 }
